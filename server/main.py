8884909e39b844dc793d6efe8869f60aef74c340
Paras Patel <paraspatel160500@gmail.com>
2026-07-17 14:44:37 +0530

    mongo health check added

--- server/main.py
+++ server/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from auth.routes import router as auth_router
 from docs.routes import router as doc_router
 from chat.routes import router as chat_router
+from pymongo import MongoClient
+import certifi
+import traceback,os
 
 app = FastAPI()
 app.include_router(auth_router)
@@ -14,3 +17,21 @@ def health_check():
         "message" : "OK"
     }
 
+@app.get("/mongo")
+def mongo_test():
+    try:
+        client = MongoClient(
+            os.getenv("MONGO_URI"),
+            tls=True,
+            tlsCAFile=certifi.where(),
+        )
+
+        client.admin.command("ping")
+        return {"status": "connected"}
+
+    except Exception as e:
+        return {
+            "error": str(e),
+            "type": str(type(e)),
+            "trace": traceback.format_exc()
+        }
