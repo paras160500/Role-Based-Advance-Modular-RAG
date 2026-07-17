d6e61d5f3c6f87f1eb78e19a86dd3b102506ceb7
Paras Patel <paraspatel160500@gmail.com>
2026-07-17 14:05:06 +0530

    mongodb client edit

--- server/config/db.py
+++ server/config/db.py
@@ -5,6 +5,7 @@
 import os 
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import certifi
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
@@ -14,6 +15,19 @@ DB_NAME = os.getenv("DB_NAME")
 #                                   Logic Statements
 #----------------------------------------------------------------------------------------
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=10000
+)
+
+# Test connection (temporary)
+try:
+    client.admin.command("ping")
+    print("MongoDB connected successfully")
+except Exception as e:
+    print("MongoDB connection failed:", e)
+
 db = client[DB_NAME]
 users_collection = db['users']
